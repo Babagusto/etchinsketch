717a3909a9824433dde256190a5ad88e0731870c
Cole Gibson <coleggibson@gmail.com>
2022-07-14 17:56:45 -0500

    Created my 16 by 16 grid

--- javascript.js
+++ javascript.js
@@ -14,13 +14,13 @@ var container = document.querySelector('#container');
 
 
 
-for(x=0; x<16; x++) {
+for(x=0; x<256; x++) {
     const squareGrid = document.createElement('div');
     squareGrid.classList.add('squareGrid');
-    squareGrid.style.border = "solid";
+    squareGrid.style.border = "solid 1px";
     squareGrid.style.padding = "20px 20px";
-    squareGrid.style.width = "120px";
-    squareGrid.style.display = "inline-block"
-   container.appendChild(squareGrid);
+    squareGrid.style.width = "20px";
+    squareGrid.style.display = "inline-block";
+    container.appendChild(squareGrid);
   
 }

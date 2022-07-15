43992d8f34659d86c8f4208d321244c82ab49a84
Cole Gibson <coleggibson@gmail.com>
2022-07-15 15:12:54 -0500

    Created trailing effect

--- javascript.js
+++ javascript.js
@@ -1,26 +1,33 @@
 var container = document.querySelector('#container');
+let square = document.querySelector('#square');
+let squareGrid = document.querySelector('#squareGrid')
 
 
+createGrid();
 
+container.addEventListener ('mouseover', function (e){
+    if (e.target.matches('.square')){
+        e.target.classList.add('hoverSquare')
+    }
+}) 
 
-
-
-
-
-
-
-
-
-
-
-
-for(x=0; x<256; x++) {
-    const squareGrid = document.createElement('div');
-    squareGrid.classList.add('squareGrid');
-    squareGrid.style.border = "solid 1px";
-    squareGrid.style.padding = "20px 20px";
-    squareGrid.style.width = "20px";
-    squareGrid.style.display = "inline-block";
+function createGrid (i){
+for(let x=0; x<16*16; x++) {
+    let squareGrid = document.createElement('div');
+    squareGrid.classList.add('square');
     container.appendChild(squareGrid);
+    
+}}
+
+    //square.addEventListener ("mouseout", hover, false )
+
+//squareGrid.addEventListener("mouseout", func1, false);
+
+
+
+
+
+
+
   
-}
+  

36db3a36c1744033bee15b18ec8b5f23bb23415a
Cole Gibson <coleggibson@gmail.com>
2022-07-18 20:28:27 -0500

    Finished the reset and gridsize buttons

--- javascript.js
+++ javascript.js
@@ -1,27 +1,51 @@
-var container = document.querySelector('#container');
+var container = document.querySelector('#grid');
 let square = document.querySelector('#square');
-let squareGrid = document.querySelector('#squareGrid')
+let submit = document.getElementById("#submit");
+let userChoice = '';
+let defaultValue = 16;
+let gridSize = userChoice || defaultValue;
+let button = document.getElementById("userBttn");
+let resetBttn = document.getElementById("resetBttn");
 
 
-createGrid();
 
-container.addEventListener ('mouseover', function (e){
+grid.addEventListener ('mouseover', function (e){
     if (e.target.matches('.square')){
         e.target.classList.add('hoverSquare')
     }
 }) 
 
-function createGrid (i){
-for(let x=0; x<16*16; x++) {
-    let squareGrid = document.createElement('div');
-    squareGrid.classList.add('square');
-    container.appendChild(squareGrid);
+button.addEventListener ("click", getSelection);
+
+resetBttn.addEventListener ("click", resetGrid);
+
+
+
+function resetGrid() {
+    location.reload();
+}
+
+function getSelection() {
+
+    userChoice = prompt("How big would you like the grid?")
+
+    if (userChoice <= 100 && userChoice % 4 === 0) {
+    createGrid(userChoice);
+    } else getSelection()
+} 
+
+
+function createGrid (userChoice){
+for(let x=0; x<Math.pow(userChoice, 2); x++) {
+    container.style.gridTemplateColumns = `repeat(${userChoice}, 1fr)`;
+    let square = document.createElement('div');
+    square.classList.add('square');
+    container.appendChild(square);
+    
     
 }}
 
-    //square.addEventListener ("mouseout", hover, false )
 
-//squareGrid.addEventListener("mouseout", func1, false);
 
 
 
